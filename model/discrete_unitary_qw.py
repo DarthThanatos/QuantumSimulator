98dc6f96da27c7c16bc861fc8d948d179282cc49
DarthThanatos <robert.bielas@interia.pl>
2019-01-29 21:18:13 +0100

    shrodringer & bloch experiments

--- model/discrete_unitary_qw.py
+++ model/discrete_unitary_qw.py
@@ -1,81 +1,24 @@
-# UNITARY_QUANTUM_WALK: Simulates discrete time quantum walk on a 1-d lattice.
-#                     : Probablity distribution plot.
-#
-# Main Variables: t           - number of steps of quantum walk.
-#               : sites       - number of lattice positions available to the walker (2*t+1).
-#               : coin_angle  - parameter of the SU(2) coin.
-#               : qubit_state - input coin state.
-#               : z (1/2)     - plot with zeros/plot without zeros.
-#
-# Output        : Plot of probablity distribution of the walker after 't' steps.
-#
-# Useful ref    : qutip - http://qutip.org/docs/3.1.0/guide/guide-basics.html
-##############################################################################################
-
 from qutip import *
 import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 
+ket0 = basis(2,0).unit( )                
+ket1 = basis(2,1).unit()                
+psip = (basis(2,0)+basis(2,1)*1j).unit() 
+psim = (basis(2,0)-basis(2,1)*1j).unit() 
 
-#Basis states
-#basis(d,n), creates a coloum vector with dimension d and n=0,1..d-1 representing the position where one appears.
-#Ex: basis(2,0): 2-d vector with '1' appearing in the 0th position [1,0]^Transpose
-ket0 = basis(2,0).unit( )                # |0>
-ket1 = basis(2,1).unit()                 # |1>
-psip = (basis(2,0)+basis(2,1)*1j).unit() # |0>+i|1>/sqrt(2); unit() is used to normalize the state
-psim = (basis(2,0)-basis(2,1)*1j).unit() # |0>-i|1>/sqrt(2)
-
-#Coin transformation
-#qutip.Qobj is a ([[]]) is a way to create a matrix or an array in qutip
-def coin(coin_angle):
-  C_hat = qutip.Qobj([[cos(radians(coin_angle)), sin(radians(coin_angle))],
-                      [sin(radians(coin_angle)), -cos(radians(coin_angle))]])
-  return C_hat
-
-#Position transformation
-def shift(t):
-  sites = 2*t+1
-  shift_l = qutip.Qobj(np.roll(np.eye(sites), 1, axis=0))  #left chairality. Roll function is a general way to realize shift operator matrix
-  shift_r = qutip.Qobj(np.roll(np.eye(sites), -1, axis=0)) #right chairality
-  S_hat = tensor(ket0*ket0.dag(),shift_l) + tensor(ket1*ket1.dag(),shift_r) #Complete shift Operator.
-  return S_hat
-
-#Walk operator: Evolution operator for DTQW
-#tensor is a function called from qutip
-def walk(t,coin_angle):
-  sites = 2*t+1
-  C_hat = coin(coin_angle)
-  S_hat = shift(t)
-  W_hat = S_hat*(tensor(C_hat,qeye(sites))) #combine both coin and shift.
-  return W_hat
-
-#Quantum walk generator: outputs the evolved wave function after 't' steps.
-#ket2dm is a qutip function used to convert kets to density matrices.
-def qwalk_gen(t,qubit_state,coin_angle):
-  sites=2*t+1
-  Position_state = basis(sites,t)                  # Initialize the lattice with num of positions = 2*t+1.
-  Psi = ket2dm(tensor(qubit_state,Position_state)) # Initial Bipartite state \psi(0)=: coin \tensor position.
-  W_hat = walk(t,coin_angle)
-  for i in range(t):                               #Apply the walk operator 't' times.
-    Psi = W_hat*Psi*W_hat.dag()
-  return Psi                                       #W^t*[\psi(0)] = \psi(t).
-
-#Projective measurement on the position basis states.
-#The walker has a zero probablity at odd positions of the lattice.
-#The zeros can be avoided if we measure the qubit only at even positions, this can be done by setting z=2.
 def measurement(t,Psi,z):
   sites=2*t+1
   print("s", sites)
   prob=[]
-  for i in range(0,sites,z):                  #This loop performs measurement at all 2*t+1 sites.
-    M_p = basis(sites,i)*basis(sites,i).dag() #Outer product.
-    Measure = tensor(qeye(2),M_p)             #Identity on coin M_p on position.
-    p = abs((Psi*Measure).tr())               #Probablity.
+  for i in range(0,sites,z):                  
+    M_p = basis(sites,i)*basis(sites,i).dag() 
+    Measure = tensor(qeye(2),M_p)             
+    p = abs((Psi*Measure).tr())               
     prob.append(p)
   return prob
 
-#Plot function
 def plot_pdf(P_p):
     lattice_positions = range(int(-len(P_p)/2+1),int(len(P_p)/2+2))
     print(lattice_positions.__len__(), P_p.__len__())
@@ -86,9 +29,37 @@ def plot_pdf(P_p):
     plt.xlabel(r'$Particle \ position$')
     plt.show()
 
+def coin(coin_angle):
+  C_hat = qutip.Qobj([[cos(radians(coin_angle)), sin(radians(coin_angle))],
+                      [sin(radians(coin_angle)), -cos(radians(coin_angle))]])
+  return C_hat
 
-# The main instance of the program
-if __name__ == "__main__":           # this line is not necessary (good practice to use though).
-  Psi_t = qwalk_gen(100,psip,45)     # call the qwalk generator.
-  P_p  = measurement(100,Psi_t,1)    # measure the wave function returned by qwalk generator.
-  plot_pdf(P_p)                      # here z=2 in the measurement func implies the measurements are made only at even positions
+def shift(t):
+  sites = 2*t+1
+  shift_l = qutip.Qobj(np.roll(np.eye(sites), 1, axis=0))  
+  shift_r = qutip.Qobj(np.roll(np.eye(sites), -1, axis=0)) 
+  print(shift_l)
+  S_hat = tensor(ket0*ket0.dag(),shift_l) + tensor(ket1*ket1.dag(),shift_r) 
+  return S_hat
+
+def walk(t,coin_angle):
+  sites = 2*t+1
+  C_hat = coin(coin_angle)
+  S_hat = shift(t)
+  U_hat = S_hat*(tensor(C_hat,qeye(sites))) 
+  return U_hat
+
+def qwalk_gen(t,qubit_state,coin_angle):
+  sites=2*t+1
+  Position_state = basis(sites,t)                  
+  Psi = ket2dm(tensor(qubit_state,Position_state)) 
+  U_hat = walk(t,coin_angle)
+  for i in range(t):                               
+    Psi = U_hat*Psi*U_hat.dag()
+  return Psi                                       
+
+
+if __name__ == "__main__":           
+  Psi_t = qwalk_gen(100,psip,45)     
+  P_p  = measurement(100,Psi_t,2)    
+  plot_pdf(P_p)                     
--- model/schodringer.py
+++ model/schodringer.py
@@ -3,11 +3,12 @@ from matplotlib.pyplot import  *
 from threading import Thread
 import time
 from math import cos, sin, radians
+import numpy as np 
 
 H = 2 * np.pi * .1 * sigmax()
 psi0 = ket("0")
-times = np.linspace(0., 10., 100)
-res = mesolve(H, psi0, times, [], [sigmaz(), sigmay()])
+times = np.linspace(0., 10., 20)
+res = mesolve(H, psi0, times, [destroy(2)], [sigmaz(), sigmay()])
 
 fig, ax = subplots()
 ax.plot(res.times, res.expect[0])
@@ -16,27 +17,61 @@ ax.set_xlabel('Time')
 ax.set_ylabel('Expectation values')
 ax.legend(["Sigma-Z", "Sigma-Y"])
 
+def normalize(vec):
+    norm = np.sqrt((vec.dag() * vec)[0,0])
+    normalized = vec * 1/norm 
+    return normalized
+
+def sampleOnSphere():
+    vec = Qobj(np.random.randn(2, 1))
+    return normalize(vec)
+
+def hamiltonianMedian(K, H):
+    res = H * sampleOnSphere() * 1. / K
+    for i in range(K-1):
+        res += H * sampleOnSphere() * 1. / K
+    return res
+
+def custom_mcsolve(H, psi0, times):
+    res = [psi0]
+    prev = psi0
+    K = 100
+    for j in range(1, times.shape[0]):
+        psi_j = normalize(prev - (times[j] - times[j-1]) * hamiltonianMedian(K, H))
+        prev = psi_j 
+        res.append(psi_j)
+        print(j)
+    return res
+
+def getEvolution(H, psi0, times):
+    res = mcsolve(H, psi0, times, [], [])
+    res = res.states
+    # res = custom_mcsolve(H, psi0, times)
+    return res
+
 def evolve():
     H = 2 * np.pi * sigmay()
     psi0 = hadamard_transform() * ket("1")
-    res = mesolve(H, psi0, times, [], [])
+    res = getEvolution(H, psi0, times)
+    print(res)
     i = 0
     xs = []
     ys = []
     zs = []
     while True:
-        if i == res.states.__len__():
+        if i == res.__len__():
             i = 0
             xs  = []
             ys = []
             zs = []
-        st = res.states[i]
+        st = res[i]
+        print(st)
         try:
             xs.append(float((st.dag() * sigmax() * st).data[0,0]))
             ys.append(float((st.dag() * sigmay() * st).data[0,0]))
             zs.append(float((st.dag() * sigmaz() * st).data[0,0]))
             b.clear()
-            b.add_states(res.states[i])
+            b.add_states(res[i])
             b.add_points([xs, ys, zs])
             b.make_sphere()
             b.fig.canvas.draw()
